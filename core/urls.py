e156414f04d3c2f68edda56259bf068f8f179968
Yehuda Freedman <hudafreedman@gmail.com>
2024-05-28 03:50:07 +0300

    more templates

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    # Home
+    path("", views.home, name="home"),
+    # Posts
     path("posts/", views.post_list, name="post_list"),
+    path("posts/raw", views.post_list_raw, name="post_list_raw"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("admin/", admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -6,10 +6,7 @@ from django.core.serializers import serialize
 
 
 def home(request):
-    qs = Post.objects.all()
-    serialized_data = serialize("json", qs)
-    serialized_data = json.loads(serialized_data)
-    return JsonResponse(serialized_data, status=200, safe=False)
+    return render(request, "core/home.html")
 
 
 def post_list(request):
@@ -38,6 +35,13 @@ def post_list(request):
 
     context = {"posts": post_list}
     return render(request, "core/post_list.html", context)
+
+
+def post_list_raw(request):
+    qs = Post.objects.all()
+    serialized_data = serialize("json", qs)
+    serialized_data = json.loads(serialized_data)
+    return JsonResponse(serialized_data, status=200, safe=False)
 
 
 def post_detail(request, post_id):
